ef89d106253a0a9844bd7ebe2900f094457970a3
Andy <andyzheng94@live.com>
2019-02-16 00:14:16 -0500

    Update app.py

--- BlogWebsiteProject/app.py
+++ BlogWebsiteProject/app.py
@@ -1,10 +1,11 @@
 from socialblogproject import app
 from datetime import datetime, timedelta
 
+# Allows datetime to be used in templates.
 @app.context_processor
 def inject_now():
 	return {'now': datetime.utcnow(), 'time':timedelta(days=2)} 
 
-
+# Runs the app.
 if __name__ == '__main__':
 	app.run()
