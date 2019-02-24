34a503cd5422bd48ca627d30aaee279ede415977
Andy <andyzheng94@live.com>
2019-02-24 03:38:24 -0500

    More updates!

--- BlogWebsiteProject/socialblogproject/users/forms.py
+++ BlogWebsiteProject/socialblogproject/users/forms.py
@@ -11,7 +11,7 @@ class LoginForm(FlaskForm):
 	submit = SubmitField('Log In')
 
 class RegistrationForm(FlaskForm):
-	email = StringField('Email (A legitimate Email is NOT required, but has to end with @youremail.com)', validators = [DataRequired(), Email()])
+	email = StringField('Email', validators = [DataRequired(), Email()])
 	username = StringField('Username', validators = [DataRequired()])
 	password = PasswordField('Password', validators = [DataRequired(), EqualTo('pass_confirm', message = 'Passwords must match!')])
 	pass_confirm = PasswordField('Confirm Password', validators = [DataRequired()])
