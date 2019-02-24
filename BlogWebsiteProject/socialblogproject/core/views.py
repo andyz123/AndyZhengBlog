f18f83912c9e4f02fedc5e8bcdf91c24d83cd21b
Andy <andyzheng94@live.com>
2019-02-24 02:34:46 -0500

    Huge Revamp PT.1
    More to come.

--- BlogWebsiteProject/socialblogproject/core/views.py
+++ BlogWebsiteProject/socialblogproject/core/views.py
@@ -5,22 +5,30 @@ core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
+	return render_template('index.html')
+
+@core.route('/blog')
+def blog():
 	page = request.args.get('page', 1, type=int)
-	blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page,per_page=10)
-	return render_template('index.html', blog_posts = blog_posts)
+	blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
+	return render_template('blog.html', blog_posts = blog_posts)
 
 @core.route('/about')
 def about(): 
 	return render_template('about.html')
 
-@core.route('/mountains')
-def mountains():
-	return render_template('mountains.html')
+@core.route('/technology')
+def technology():
+	return render_template('technology.html')
 
-@core.route('/nature')
-def nature():
-	return render_template('nature.html')
+@core.route('/audiogear')
+def audiogear():
+	return render_template('audiogear.html')
 
 @core.route('/animals')
 def animals():
 	return render_template('animals.html')
+
+@core.route('/nature')
+def nature():
+	return render_template('nature.html')
