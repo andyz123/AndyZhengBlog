31e40546608a70f6fe4da3cb762fed466877b0ad
Andy <andyzheng94@live.com>
2019-03-14 02:01:30 -0400

    Switched out SQLite for MySQL

--- BlogWebsiteProject/socialblogproject/__init__.py
+++ BlogWebsiteProject/socialblogproject/__init__.py
@@ -2,14 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secretkey'
-# Database setup
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 
 db = SQLAlchemy(app)
